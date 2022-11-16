1970586bc893249e80540ed1f6544a017dc72c07
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-16 15:21:49 +0300

    style: changed some properties

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import Pics from "../resources/pics.png"
 
 const Hero = () => {
   return (
-    <div className="container m-8 mt-12 grid grid-cols-1 md:grid-cols-2">
-      <div>
-        <p className="w-[646px] h-[148px] font-redRose font-normal text-[56px] leading-[140%] tracking-[-0.02em] text-[#434343]">
+    <div className="container mx-auto  grid grid-cols-1 md:grid-cols-2">
+      <div className="m-5">
+        <p className=" font-redRose font-normal text-5xl leading-[140%] tracking-[-0.02em] text-[#434343]">
           Rent a <span className="text-[#A02279] font-bold">Place</span> away
           from
           <br /> <span className="text-[#A02279] font-bold">Home</span> in the{" "}
           <span className="text-[#A02279] font-bold">Metaverse</span>
         </p>
         <section>
-          <p className="font-redRose text-[#434343] w-[636px] h-[102px] font-normal not-italic text-sm leading-[35px]">
+          <p className="font-redRose text-[#434343] font-normal not-italic text-sm leading-[35px]">
             Home Place to stay NFTs Community Connect wallet Rent a Place away
             from Home in the Metaverse we provide you access to luxury and
             affordable houses in the metaverse, get a chance to turn your
@@ -22,10 +23,10 @@ const Hero = () => {
           <div class="flex ">
             <input
               type="text"
-              class="w-[390px] h-[54px] top-[346.08px] p-5 rounded-tl-[8px] rounded-tr-[0px] rounded-br-[0px] rounded-bl-[8px] border border-[#a3a3a3] placeholder:font-redRose placeholder:font-normal placeholder:not-italic text-[14px] placeholder:text-[#B8B8B8] placeholder:leading-[130%]"
+              class="w-96 h-[54px] top-[346.08px] p-5 rounded-tl-[8px] rounded-tr-[0px] rounded-br-[0px] rounded-bl-[8px] border border-[#a3a3a3] placeholder:font-redRose placeholder:font-normal placeholder:not-italic text-[14px] placeholder:text-[#B8B8B8] placeholder:leading-[130%] focus:outline-none"
               placeholder="Search for location"
             />
-            <button class="w-[230px] h-[54px] text-white bg-[#A02279] rounded-tl-[0px] rounded-tr-[8px] rounded-br-[8px] rounded-bl-[0px] ">
+            <button class="w-48 h-[54px] text-white bg-[#A02279] rounded-tl-[0px] rounded-tr-[8px] rounded-br-[8px] rounded-bl-[0px] ">
               <span className="font-redRose font-normal text-[16px] leading-[19px]">
                 Search
               </span>
@@ -33,7 +34,10 @@ const Hero = () => {
           </div>
         </section>
       </div>
-      <div></div>
+      <div className="m-5 w-[25rem]">
+        <img src={Pics} alt="" />
+      </div>
+
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,87 @@
-import React from 'react'
-import house from '../resources/Vector-1.png'
-import meta from '../resources/Vector.png'
+import React, { useState } from "react";
+import logo from "../resources/logo.svg";
+import Modal from "./Modal";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineClose } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  const handleClose = () => setNav(!nav);
   return (
-    <div className='flex justify-between items-center container m-8 font-redRose'>
-      <div className='flex'>
-        <img className='w-[41.99px] h-[36.37px] top-[3.67px]'  src={house} alt="" />
-        <img className='w-[188.1px] h-[32.19px] top-[8.14px] left-[44.9px]'  src={meta} alt="" />
-        {/* <p className='text-center text-3xl font-extrabold bg-[91.47deg, #DC3E88 18.25%, #B840AE 43.34%, #843FE8 78.18%)]'>Metabnb</p> */}
+    <div className="flex justify-between items-center container mt-8 font-redRose">
+      <div className="flex gap-[10rem] m-5">
+        <section>
+          <img className="w-32 md:w-full" src={logo} alt="" />
+        </section>
+        <div className="md:hidden mr-4 z-10" onClick={handleClick}>
+          {!nav ? (
+            <GiHamburgerMenu className="w-5" />
+          ) : (
+            <MdOutlineClose className="w-5" />
+          )}
+        </div>
       </div>
-      <div>
-        <ul className='flex gap-4'>
-          <li>Home</li>
-          <li>Place to stay</li>
-          <li>NFTs</li>
-          <li>Community</li>
+      <div className="hidden md:flex">
+        <ul className="flex gap-4">
+          <a href="http://">
+            <li>Home</li>
+          </a>
+          <a href="http://">
+            <li>Place to stay</li>
+          </a>
+          <a href="http://">
+            <li>NFTs</li>
+          </a>
+          <a href="http://">
+            <li>Community</li>
+          </a>
         </ul>
       </div>
-      <div>
-        <button className='w-[170px] h-[48px] left-[1070px] gap-[10px] bg-[#A02279] rounded-[10px] font-redRose not-italic font-normal text-[16px] leading-5 text-white'>Connect wallet</button>
+      <div className="hidden md:flex">
+        <button
+          onClick={() => setShowModal(true)}
+          className="w-[170px] h-[48px] left-[1070px] gap-[10px] bg-[#A02279] rounded-[10px] font-redRose not-italic font-normal text-[16px] leading-5 text-white"
+        >
+          Connect wallet
+        </button>
+        {showModal ? <Modal setShowModal={setShowModal} /> : null}
       </div>
-    </div>
-  )
-}
 
-export default Navbar
+      <ul
+        className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8 mt-60"}
+      >
+        <ul className="flex flex-col gap-4">
+          <a href="http://">
+            <li>Home</li>
+          </a>
+          <a href="http://">
+            <li>Place to stay</li>
+          </a>
+          <a href="http://">
+            <li>NFTs</li>
+          </a>
+          <a href="http://">
+            <li>Community</li>
+          </a>
+        </ul>
+
+
+
+
+        <div className="flex flex-col my-4">
+        <button
+          onClick={() => setShowModal(true)}
+          className="w-[170px] h-[48px] left-[1070px] gap-[10px] bg-[#A02279] rounded-[10px] font-redRose not-italic font-normal text-[16px] leading-5 text-white"
+        >
+          Connect wallet
+        </button>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
