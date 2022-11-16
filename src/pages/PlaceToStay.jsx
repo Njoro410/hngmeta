92fdc07bff2d39164ae2e9406b5c4dca35a471e1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-16 22:12:22 +0300

    feat: added overflow for submenu in PlaceToStay

--- src/pages/PlaceToStay.jsx
+++ src/pages/PlaceToStay.jsx
@@ -6,7 +6,7 @@ const PlaceToStay = () => {
   return (
     <div>
       <div className="flex justify-center items-center gap-12 m-5">
-        <ul className="flex gap-12 font-redRose not-italic font-normal text-lg leading-6 text-[#434343]">
+        <ul className="flex overflow-y-scroll md:overflow-y-hidden gap-12 font-redRose not-italic font-normal text-lg leading-6 text-[#434343]">
           <li>Restaurant</li>
           <li>Cottage</li>
           <li>Castle</li>
