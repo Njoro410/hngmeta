445e42cb65b230062337e63d5ed2d53b8d1288b4
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-16 15:20:53 +0300

    Feat: Added Call to action

---
+++ src/components/CTA.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import skewed from "../resources/skewed.png"
+const CTA = () => {
+  return (
+    <div className="w-full h-[550px] bg-[#A02279] grid grid-cols-1 md:grid-cols-2">
+      <div className="container m-auto ml-8">
+        <p className="font-redRose font-bold text-5xl leading-[3.75rem] text-white left-[100px] top-[186px]">Metabnb NFTs</p>
+        <p className="font-redRose font-normal text-lg text-white mb-4">
+          Discover our NFT gift cards collection. Loyal customers gets amazing
+          gift cards which are traded as NFTs. These NFTs gives our cutomer
+          access to loads of our exclusive services.
+        </p>
+        <a href="#" className="bg-white rounded-[8px] px-3 py-3"><span className="font-redRose text-[#A02279] font-normal text-lg">Learn more</span></a>
+      </div>
+      <div className="flex items-center justify-center">
+        <img src={skewed} className="w-[35rem]" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default CTA;
